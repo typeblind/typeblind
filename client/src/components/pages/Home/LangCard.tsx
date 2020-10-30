c73a225c63cc721c7c82e71304881a059f1e7b32
KletskovG <gfgfddgleb@gmail.com>
2020-10-30 14:26:48 +0300

    Update home page cards

--- client/src/components/pages/Home/LangCard.tsx
+++ client/src/components/pages/Home/LangCard.tsx
@@ -3,29 +3,47 @@ import { css } from '@emotion/core';
 import Page from '../../layout/Page';
 import { useLocation } from 'wouter';
 
-type LangCard = {
+export type LangCardProps = {
   name: string;
-  color?: string;
+  color: string;
   extension: string;
+  description: string;
 }
 
-const LangCard: FC<LangCard> = ({ name, color, extension }) => {
+const LangCard: FC<LangCardProps> = ({ name, color, extension, description }) => {
   const [,setLocation] = useLocation();
   const cardStyles = css`
+    border-radius: 5px;
+    border-left: 5px solid ${color};
+    padding-left: 24px;
+    background: white;
+    width: 30%;
+    color: ${color};
+    text-align: left;
+    cursor: pointer;
+    margin-bottom: 24px;
 
+    .title {
+      font-size: 2rem;
+    }
+
+    .description {
+      font-size: 0.9rem;
+    }
   `;
   
   return (
     <>
-      <Page>
-        <div
-          onClick={() => {
-            setLocation(`/type/${name}`)
-          }}
-        >
-          { name }
-        </div>
-      </Page>
+      <div
+        onClick={() => {
+          setLocation(`/type/${name}`)
+        }}
+        css={cardStyles}
+      >
+        <p className={"title"}> { name } </p>
+        <p className={"extension"}> { extension } </p>
+        <p className={"desc"}> { description } </p>
+      </div>
     </>
   )
 }

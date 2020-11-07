cd796cbef2cdd7f0d06fc0051588dd17666c94c2
kletskovg <gfgfddgleb@gmail.com>
2020-11-07 03:45:36 +0300

    Make first requests to github api

--- server/src/internal/github/getFile.go
+++ server/src/internal/github/getFile.go
@@ -3,24 +3,58 @@ package github
 import (
 	"context"
 	"fmt"
-	// "net/http"
 	"github.com/google/go-github/github"
-	// "golang.org/x/oauth2"
+	"golang.org/x/oauth2"
+	"os"
 )
 
+var (
+	token = os.Getenv("GITHUB_TOKEN")
+)
+
+type TokenSource struct {
+	AccessToken string
+}
+
+func (t *TokenSource) Token() (*oauth2.Token, error) {
+	token := &oauth2.Token{
+			AccessToken: t.AccessToken,
+	}
+	return token, nil
+}
+
+
 func GetFile(extension string) string {
- 	// search := github.SearchService()
-	 // users := search.Users(context.Background(), "")
+	ctx := context.Background()
 	client := github.NewClient(nil)
-	options := &github.SearchOptions{Sort: "created", Order: "desc", TextMatch: true}
-	result, response, error := client.Search.Code(context.Background(), extension, options)
-	// result := client.Search.
-	if (error != nil) {
-		fmt.Println(result.CodeResults)
-		fmt.Println(result.GetIncompleteResults())
-		fmt.Println(result.IncompleteResults)
-		fmt.Println(response)
+	repos, _, err := client.Repositories.List(ctx, "", nil)
+
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+	} else {
+		fmt.Println(repos)
 	}
 
+	files, _, err := client.Search.Code(ctx, "addClass+in:file+language:js+repo:jquery/jquery", &github.SearchOptions{TextMatch: true})
+	if (err !=nil) {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+	} else {
+		fmt.Println(files.CodeResults)
+	}
+
+	result, _, err := client.Organizations.ListAll(ctx, nil)
+
+	if (err != nil) {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	}
+
+	fmt.Println("ORgs")
+	fmt.Println(result[0])
+	fmt.Println(len(result))
  	return "Hello"
 }

08cd2ab57afa8c2d0a61b3d32ef1bae153fb92d0
roman <rdbogdanovskiy@miem.hse.ru>
2020-11-26 22:55:07 +0300

    gitflow-#3: fix mr comments

--- client/src/components/pages/Type/Editor/CurrentRow.tsx
+++ client/src/components/pages/Type/Editor/CurrentRow.tsx
@@ -8,13 +8,6 @@ type CurrentRowType = {
 
 export const CurrentRow: FC<CurrentRowType> = ({row}) => {
     
-    // useEffect(() => {
-    //     if (active) {
-    //       setClasses([...classes, 'newClass'])
-    //     }
-      
-    //   }, [curChar, active])
-
     return (
         <div>
         <pre>

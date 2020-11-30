a387ddacdccd13ecf05d162c64c5a111e36a012d
roman <rdbogdanovskiy@miem.hse.ru>
2020-11-30 20:32:56 +0300

    Test commit 3

--- client/src/components/pages/Type/Editor/Line.tsx
+++ client/src/components/pages/Type/Editor/Line.tsx
@@ -1,53 +1,35 @@
 import React, { FC, useEffect, useRef, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { DefaultParams, useLocation, useRoute } from "wouter";
-import { RootState } from "../../../../store";
-import { ChangeChar, ChangeRow } from "../../../../store/editor/actions";
 import { hljs } from "../../../../utils/";
-import { Char } from "./Char";
-import Highlight from "react-highlight.js";
 
 type LineProps = {
   line: string[];
   id: number;
   executed: boolean;
-  current: boolean
+  current: boolean;
 };
 
 // Line of code
 const Line: FC<LineProps> = ({ line, id, executed, current }) => {
   const lineRef = useRef(null);
   const [match, params] = useRoute("/type/:language/:extension");
-  const [errorChar, setErrorChar] = useState(false);
-  const [newLineWithEnter, setnewLineWithEnter] = useState<string[]>([]);
-  const curChar = useSelector((state: RootState) => state.editor.curChar);
-  const boardChar = useSelector((state: RootState) => state.editor.boardChar);
-  const dispatch = useDispatch();
-  // const newLine = line.filter((char) => char !== "\t" && char !== "\n");
 
   useEffect(() => {
     hljs.hlBlock(document.querySelector(`#codes-${id}`) as HTMLElement);
   }, []);
 
-
-
   return (
-    // <Highlight language={`${params?.language}`}>
-    <>
-    {/* {curChar} */}
-     <div id={`codes-${id}`} className={`hljs ${params?.language}`}>
-       <pre>
-      {line.map((char, index) => {
-        return (
-          <span className={executed ? "executed" : ""} key={index}>
-            {char}
-          </span>
-        );
-      })}
+    <div id={`codes-${id}`} className={`hljs ${params?.language}`}>
+      <pre>
+        {line.map((char, index) => {
+          return (
+            <span className={executed ? "executed" : ""} key={index}>
+              {char}
+            </span>
+          );
+        })}
       </pre>
-      </div>
-    {/* </Highlight> */}
-    </>
+    </div>
   );
 };
 
